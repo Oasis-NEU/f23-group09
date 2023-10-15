11da56ee2a2f8a99194267c458dfa9173c416321
reesecantu <65140498+reesecantu@users.noreply.github.com>
2023-10-15 13:42:44 -0400

    Update supabase.js

--- lets-go/src/supabase.js
+++ lets-go/src/supabase.js
@@ -4,4 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vtieflsbjzhxsfhjmdms.supabase.co'
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 
+console.log(process.env.REACT_APP_SUPABASE_KEY);
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
