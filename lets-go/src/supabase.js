6b1ab1efbbedb2a738dcde0aae71a2999a6751c7
reesecantu <65140498+reesecantu@users.noreply.github.com>
2023-10-22 12:49:47 -0400

    Shift over to react native

--- lets-go/src/supabase.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vtieflsbjzhxsfhjmdms.supabase.co'
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
