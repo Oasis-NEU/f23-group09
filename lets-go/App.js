f7f95fc983ee1739cae78a43b3179352f00387d0
reesecantu <65140498+reesecantu@users.noreply.github.com>
2023-11-05 01:31:22 -0400

    Creating SelectionScreen in appropriate file

--- lets-go/App.js
+++ lets-go/App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  SafeAreaView, 
+  Platform,
+  Dimensions
+} from 'react-native';
+import { SelectionScreen } from './app/Screens/SelectionScreen';
+
 
 export default function App() {
+  console.log(Dimensions.get("screen"))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+    <SelectionScreen />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#8fb35b',
+
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+
+  baseText: {
+    color: 'white',
+    fontFamily: 'Futura-Medium',
+    fontWeight: 'bold'
+  }
 });
---
+++ lets-go/app/Screens/SelectionScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ImageBackground, StyleSheet} from 'react-native';
+
+function SelectionScreen(props) {
+    return(
+        <Text>"hi"</Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: "100%",
+        height: "100%",
+    },
+})
+
+export default SelectionScreen;
