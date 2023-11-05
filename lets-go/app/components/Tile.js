461d074ff10b8e8e911f94e354b88995d7424a12
reesecantu <65140498+reesecantu@users.noreply.github.com>
2023-11-05 01:27:15 -0500

    Created an event tile for selection page

---
+++ lets-go/app/components/Tile.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+const Tile = ({ name, date, image }) => {
+  return (
+    <View style={styles.tileContainer}>
+      <Image source={{ uri: image }} style={styles.image} />
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.date}>{date}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tileContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 8,
+    padding: 16,
+    backgroundColor: 'white',
+    borderRadius: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  name: {
+    marginTop: 8,
+    fontWeight: 'bold',
+  },
+  date: {
+    marginTop: 4,
+    color: 'gray',
+  },
+});
+
+export default Tile;
